a014d64b80d128cb61873d4c27d448c83da4f3ba
borntojesus <borntojesus@gmail.com>
2024-02-15 19:25:50 +0200

    add array

---
+++ 06_Array/js/main.js
@@ -0,0 +1,57 @@
+
+
+
+const cars = [
+    {
+        img: './img/volkswagen.webp',
+        brand: 'Volkswagen',
+        model: 'Touran',
+        age: 2003,
+        price: 4700,
+        lenght: 267,
+    },
+    {
+        img: './img/volkswagen.webp',
+        brand: 'Volkswagen',
+        model: 'Passat',
+        age: 2019,
+        price: 12700,
+        lenght: 106,
+    },
+    {
+        img: './img/volkswagen.webp',
+        brand: 'Mersedes',
+        model: 'E',
+        age: 2013,
+        price: 22700,
+        lenght: 267,
+    },
+    {
+        img: './img/volkswagen.webp',
+        brand: 'Mersedes',
+        model: 'S',
+        age: 2015,
+        price: 33700,
+        lenght: 67,
+    },
+    {
+        img: './img/volkswagen.webp',
+        brand: 'Mazda',
+        model: '6',
+        age: 2010,
+        price: 11700,
+        lenght: 190,
+    },
+    {
+        img: './img/volkswagen.webp',
+        brand: 'Mazda',
+        model: '3',
+        age: 2006,
+        price: 3700,
+        lenght: 230,
+    },
+
+
+]
+
+console.log(cars);
